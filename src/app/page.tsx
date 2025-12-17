a09df4594ee1d6234b5a692489132b3b16a5fe18
Almudena Guerras De Diego <almudenaguerrasdediego@192.168.0.121>
2025-12-17 15:15:54 +0100

    refactor: move Todo page to Home root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import FormTodo from "@/app/todo/components/FormTodo";
+import ListTodo from "@/app/todo/components/ListTodo";
+import { prisma } from "@/lib/prisma";
+
+// Server Component
+const TodoPage = async () => {
+  const todos = await prisma.todo.findMany();
+
+  return (
+    <div className="space-y-6">
+      <h1 className="text-center text-3xl my-10">Todo List</h1>
+      <FormTodo />
+      <ListTodo todos={todos} />
+    </div>
+  );
 };
-export default HomePage;
+export default TodoPage;
